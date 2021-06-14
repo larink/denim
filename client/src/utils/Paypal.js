e2cf1bf365b75ab4d10ebfb8ed4d07a10d267988
larink <kirya.larin.1999@mail.ru>
2021-06-14 08:52:58 +0300

    prepare for heroku

--- client/src/utils/Paypal.js
+++ client/src/utils/Paypal.js
@@ -35,16 +35,11 @@ export default class Paypal extends React.Component {
         onSuccess={onSuccess}
         onCancel={onCancel}
         style={{
-<<<<<<< HEAD
-          size: 'large',
-          color: 'blue',
-=======
           size: 'medium',
           color: 'black',
           shape: 'rect',
           label: 'paypal',
           tagline: false,
->>>>>>> parent of 5115327 (Merge pull request #1 from larink/server)
         }}
       />
     )
--- server/config/index.js
+++ server/config/index.js
@@ -7,4 +7,5 @@ export default {
   MONGO_URI: process.env.MONGO_URI,
   MONGO_DB_NAME: process.env.MONGO_DB_NAME,
   JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 };
